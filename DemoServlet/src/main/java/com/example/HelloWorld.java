f20d5c88ba39468aaf99eb919532330d99d8107b
Rasy Chum <chumr168@gmail.com>
2026-08-18 11:17:36 +0700

    Servlet

---
+++ DemoServlet/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+    	PrintWriter out = response.getWriter();
+    	out.println("<html>");
+    	out.println("<head><title>Hello World</title></head>");
+    	out.println("<body>");
+    	out.println("<h1>Servlet Section</h1>");
+    	out.println("<p>Welcome to our first Servlet Section.</p>");
+    	out.println("</body>");
+    	out.println("</html>");
+		
+	}
+
+}
